e4318b761a13f0472f54f48877daa2b152078b56
Guilherme-mendesc <guimendes1337@gmail.com>
2023-03-06 12:29:11 -0300

    att

---
+++ src/application/program.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.db.Department;
+
+public class program {
+
+	public static void main(String[] args) {
+		// parei na aula 277
+		
+		Department dp1 = new Department(1, "test");
+		
+		System.out.println(dp1);
+
+	}
+
+}
