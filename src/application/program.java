a6831afac0648eb7706664a22417404cc7a93237
Guilherme-mendesc <guimendes1337@gmail.com>
2023-03-07 08:18:42 -0300

    class seller

--- src/application/program.java
+++ src/application/program.java
@@ -1,15 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.db.Department;
+import model.db.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 		// parei na aula 277
 		
-		Department dp1 = new Department(1, "test");
+		Department obj = new Department(1, "test");
 		
-		System.out.println(dp1);
+		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0 , obj);
+		
+		System.out.println(seller);
+		System.out.println(obj);
 
 	}
 
